fe10bbb535e46a6cbc0b96d5372d8614ecde3497
(no author) <(no author)@fe871d7c-bc1d-0410-af38-a64392ef1561>
2008-07-28 22:36:55 +0000

    Wrote documentation for controller_plugin.rb and view_plugin.rb
    git-svn-id: svn://192.168.1.5/copious/restful_captcha@5750 fe871d7c-bc1d-0410-af38-a64392ef1561

--- lib/restful_captcha/rails/controller_plugin.rb
+++ lib/restful_captcha/rails/controller_plugin.rb
@@ -1,8 +1,13 @@
+# == Synopsis
+#
+# Extends ActionController::Base with methods for retrieving captchas
+# and specifying attributes of the captchas to retrieve.
+
 module RestfulCaptcha
   module Rails
     module ControllerPlugin
       
-      def self.included(base)
+      def self.included(base)  # :nodoc:
         base.helper_method :captcha, :reset_captcha
         base.extend(ClassMethods)
         @@captcha_host ||= nil
@@ -11,6 +16,9 @@ module RestfulCaptcha
 
       module ClassMethods
 
+        # Gives RestfulCaptcha::Rails::Captcha a host to use as a
+        # RestfulCaptcha server. RestfulCaptcha::Rails::Captcha
+        # methods will generally not function until a host is set.
         def set_captcha_host(host)
           RestfulCaptcha::Rails::Captcha.host = host
         end
@@ -28,16 +36,26 @@ module RestfulCaptcha
       
       private
 
+      # Returns the captcha associated with the current user. A new
+      # captcha can be associated with the user by calling
+      # ActionController::Base#reset_captcha
       def captcha
         @captcha ||= Captcha.find_by_identifier(session[:captcha])
       end
 
+      # Associates a new captcha with the current user. This is useful
+      # if the user has already answered, or attempted to answer, a
+      # previously associated captcha; or if no captcha has been
+      # associated with the user yet.
       def reset_captcha
         @captcha = Captcha.find_by_attributes(@@captcha_params)
         session[:captcha] = @captcha.identifier
         return @captcha
       end
 
+      # Removes any captcha associated with the current user. Calls to
+      # ActionController::Base#captcha made after this will return nil
+      # until ActionController::Base#reset_captcha is called.
       def unset_captcha
         session[:captcha] = nil
         @captcha = nil
--- lib/restful_captcha/rails/view_plugin.rb
+++ lib/restful_captcha/rails/view_plugin.rb
@@ -1,10 +1,19 @@
 module RestfulCaptcha
   module Rails
     module ViewPlugin
+
+      # Returns an HTML image tag with an href pointing to the image
+      # url of the captcha associated with the current user. This
+      # method calls ActionController::Base#reset_captcha; so a
+      # different captcha is shown every time this method is called.
+      #
+      # The optional +options+ hash is passed to the image_tag method,
+      # which is used to generate the HTML tag for the displayed image.
       def captcha_tag(options={})
         reset_captcha
         image_tag(captcha.image_url, options.reverse_merge(:alt => 'captcha image'))
       end    
+
     end
   end
 end
