0654c8e2f18bc0d7c1c8b5e00891ca6e9f7023b5
(no author) <(no author)@fe871d7c-bc1d-0410-af38-a64392ef1561>
2008-07-28 21:47:51 +0000

    * Wrote documentation for restful_captcha.rb and RestfulCaptcha::Captcha * Extended encryption and decryption methods to handle keys of any length
    git-svn-id: svn://192.168.1.5/copious/restful_captcha@5745 fe871d7c-bc1d-0410-af38-a64392ef1561

--- lib/restful_captcha/captcha.rb
+++ lib/restful_captcha/captcha.rb
@@ -1,3 +1,9 @@
+# RestfulCaptcha::Captcha represents a Captcha. It has text, image
+# properties, and image, and possibly a secret as attributes.
+#
+# Once a Captcha is created, its image can be accessed by calling
+# captcha.image
+
 require 'activesupport'
 require 'crypt/rijndael'
 
@@ -5,6 +11,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../core_extensions.rb')
 
 module RestfulCaptcha
   class Captcha
+
+    # A new Captcha accepts +options+ than define how its image will
+    # appear. The options that are accepted are:
+    # * +text+ - the text displayed in the captcha image; will be randomly generated if not specified
+    # * +width+, +height+ - dimensions of the image in pixels; defaults to 200x100
+    # * +color+, +background_color+ - accpeted color values are described at http://www.imagemagick.org/RMagick/doc/imusage.html#color_names
+    # * +background+ - used to specify a background texture instead of a solid color; overrides background color if specified; accepted values are described at http://www.imagemagick.org/RMagick/doc/imusage.html#builtin_formats
+    # * +font+, +font_family+, +font_style+, +font_weight+, +font_size+ - font properties; see http://www.imagemagick.org/RMagick/doc/draw.html#font for info
+    # * +secret+ - a string to be hidden in the captcha. It will be returned to the client in the event a correct answer is submitted.
 
     def initialize(options={})
       options = options.symbolize_keys
@@ -14,6 +29,14 @@ module RestfulCaptcha
       @secret = options[:secret]
     end
 
+    # Given a Captcha identifier, returns the corresponding captcha.
+    #
+    # Behind the scenes, the identifier is actually a YAML file
+    # containing the properties of the desired captcha that has been
+    # encrypted, compressed, and encoded in hexadecimal. This method
+    # converts it back to binary, decompresses it, decrypts it, and
+    # creates a new captcha with the properties listed in the
+    # resulting YAML file.
     def self.find(identifier)
       recipe = YAML::load(decrypt(Zlib::Inflate.inflate(hex_to_bin(identifier))))
       Captcha.new(recipe)
@@ -21,26 +44,49 @@ module RestfulCaptcha
       nil
     end
 
+    # Returns the captcha's identifier, which can be used to retrieve
+    # the captcha later.
+    #
+    # Behind the scenes, the identifier is actually a YAML file
+    # containing the properties of the desired captcha that has been
+    # encrypted, compressed, and encoded in hexadecimal.
     def identifier
       identifier_params = @recipe.reject { |k,v| v.nil? }.stringify_keys
       identifier_params[:secret] = @secret unless @secret.blank?
       bin_to_hex(Zlib::Deflate.deflate(encrypt(identifier_params.to_yaml)))
     end
 
+    # Returns the captcha image in PNG format. 
+    #
+    # RMagick is used to actually build the the image given the
+    # captcha's attributes.
     def image
       Image.build(@recipe)
     end
 
+    # Returns true if +answer+ matches the captcha's text, and false otherwise.
+    #
+    # By default the comparison between +answer+ and the captcha's
+    # text is not case sensitive. Pass +true+ as the second argument
+    # to make the check case sensitive.
     def correct_answer?(answer, case_sensitive=false)
       correct_answer = case_sensitive ? self[:text] : self[:text].downcase
       answer = answer.downcase unless case_sensitive
       !answer.blank? and correct_answer == answer
     end
 
+    # Retrieves one of the captcha's attributes by name. +attribute+
+    # should be a Symbol or a String.
+    #
+    # See RestfulCaptcha::Captcha#initialize for a list of attributes.
     def [](attribute)
       attribute.to_sym == :secret ? @secret : @recipe[attribute.to_sym]
     end
 
+    # Sets one of the captchas attributes by name. +attribute+ should
+    # be a Symbol or a String.
+    #
+    # See RestfulCaptcha::Captcha#initialize for a list of attributes.
     def []=(attribute, value)
       if attribute.to_sym == :secret
         @secret = value
@@ -51,6 +97,8 @@ module RestfulCaptcha
       end
     end
 
+    # Returns true if the receiver and +other_captcha+ have identical
+    # attributes, false otherwise.
     def ==(other_captcha)
       @recipe.each do |k,v|
         return false if other_captcha[k] != v
@@ -60,6 +108,8 @@ module RestfulCaptcha
     
     private
 
+    # Generates a random string containing upper- and lower-case
+    # letters and numbers with the given +length+.
     def random_string(length=5)
       chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
       string = ""
@@ -67,18 +117,30 @@ module RestfulCaptcha
       return string
     end
 
+    # Encrypts +message+ use the Rijndael implementation of AES. 
+    #
+    # The encryption key is read from the file, 'keyfile', which
+    # should be placed in the top-level directory of the
+    # application. This is a symmetric encryption algorithm; so the
+    # same key is used for encryption and decryption.
+    #
+    # Encryption uses ECB for encrypting multiple blocks instead of
+    # the more widely used CBC. Using ECB results in significantly
+    # shorter encrypted strings when the input string is short, but at
+    # the expense of somewhat weaker encryption.
     def encrypt(message)
       key = File.open('keyfile','r') { |f| f.read }
+      block_length = key.length
       rijndael = Crypt::Rijndael.new(key)
 
-      # split message into 16 byte blocks
-      blocks = Array.new((message.length / 16.0).ceil).collect_with_index do |b,i|
-        b = message[i*16..i*16+15]
+      # split message into blocks with the same length as the encryption key
+      blocks = Array.new((message.length.to_f / block_length.to_f).ceil).collect_with_index do |b,i|
+        b = message[i*block_length..(i*block_length)+block_length-1]
       end
 
-      # pad the last block with null characters to fill it out to 16
-      # bytes
-      while blocks.last.length < 16
+      # pad the last block with null characters to fill it out to
+      # block_length bytes
+      while blocks.last.length < block_length
         blocks.last << "\0"
       end
 
@@ -90,13 +152,25 @@ module RestfulCaptcha
 
     class << self
       
+      # Decrypts +message+ use the Rijndael implementation of AES. 
+      #
+      # The decryption key is read from the file, 'keyfile', which
+      # should be placed in the top-level directory of the
+      # application. This is a symmetric encryption algorithm; so the
+      # same key is used for encryption and decryption.
+      #
+      # Encryption uses ECB for encrypting multiple blocks instead of
+      # the more widely used CBC. Using ECB results in significantly
+      # shorter encrypted strings when the input string is short, but
+      # at the expense of somewhat weaker encryption.
       def decrypt(message)
         key = File.open('keyfile','r') { |f| f.read }
+        block_length = key.length
         rijndael = Crypt::Rijndael.new(key)
         
-        # split message into 16 byte blocks
-        blocks = Array.new((message.length / 16.0).ceil).collect_with_index do |b,i|
-          b = message[i*16..i*16+15]
+        # split message into blocks with the same length as the encryption key
+        blocks = Array.new((message.length.to_f / block_length.to_f).ceil).collect_with_index do |b,i|
+          b = message[i*block_length..(i*block_length)+block_length-1]
         end
         
         blocks.collect { |b| rijndael.decrypt_block(b) }
@@ -110,20 +184,31 @@ module RestfulCaptcha
       
     end
 
+    # Encodes a string, possibly containing binary data, in
+    # hexadacimal - which is a nice safe encoding for use in a URL.
     def bin_to_hex(bin)
       bin.unpack('H*').first
     end
 
+    # Encodes a string, possibly containing binary data, in base64
+    # encoding. This results in shorter encoded strings than
+    # hexadecimal; but it has the disadvantages that encoded strings
+    # often contain characters that are not safe for URL parameters,
+    # and the encoded string is case sensitive.
     def base64_encode(bin)
       [bin].pack('m')
     end
 
     class << self
 
+      # Converts a hexadecimal encoded string back to its original
+      # representation - which can be binary.
       def hex_to_bin(hex)
         [hex].pack('H*')
       end
       
+      # Converts a base64 encoded string back to its original
+      # representation - which can be binary.
       def base64_decode(base64)
         base64.unpack('m').first
       end
--- restful_captcha.rb
+++ restful_captcha.rb
@@ -1,4 +1,53 @@
 #!/usr/bin/env ruby
+#
+# This is a server that provides access to Captcha images and that
+# verifies answers to Captcha challenges.
+# 
+# Captchas can be requested by attributes such as text, color, and
+# font properties.
+#
+# The server exposes a RESTful API that allows read-only access to
+# Captchas and their attributes. It responds to URLs in four formats:
+#
+### /captcha
+#
+# Finds a captcha that matches the given parameters and returns its
+# identifier in the response body. If no parameters are given,
+# responds with an identifier for a captcha picked at random.
+#
+# The parameters that are used are:
+# * +text+ - the text displayed in the captcha image; will be randomly generated if not specified
+# * +width+, +height+ - dimensions of the image in pixels; defaults to 200x100
+# * +color+, +background_color+ - accpeted color values are described at http://www.imagemagick.org/RMagick/doc/imusage.html#color_names
+# * +background+ - used to specify a background texture instead of a solid color; overrides background color if specified; accepted values are described at http://www.imagemagick.org/RMagick/doc/imusage.html#builtin_formats
+# * +font+, +font_family+, +font_style+, +font_weight+, +font_size+ - font properties; see http://www.imagemagick.org/RMagick/doc/draw.html#font for info
+# * +secret+ - a string to be hidden in the captcha. It will be returned to the client in the event a correct answer is submitted.
+#
+### /captcha/:identifier
+#
+# Given a captcha identifier, returns the same identifier in the
+# response body with an OK status; or responds with a 'resource not
+# found' error if the captcha cannot be found.
+#
+# This is useful for clients wanting to verify that an identifier is
+# valid.
+#
+### /captcha/:identifier/image
+#
+# Given a captcha identifier, returns the captcha's image in PNG format.
+#
+### /captcha/:identifier/:answer
+#
+# Given a captcha identifier and a proposed answer, returns the
+# validity of the answer. The response is in the format of a YAML
+# file, where the validity of the answer is represented as a boolean
+# with the key 'correct'.
+#
+# The response also includes the captcha identifier and the answer
+# given, with the keys 'identifier' and 'answer' respectively.
+#
+# If the given answer is correct, the YAML response also includes the
+# secret string embedded in the captcha, if there is one.
 
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
@@ -11,20 +60,11 @@ require 'yaml'
 require 'lib/restful_captcha/image'
 require 'lib/restful_captcha/captcha'
 
-# Finds a captcha that matches the given parameters and returns its
-# identifier in the response body. If no parameters are given,
-# responds with an identifier for a captcha picked at random.
 get '/captcha' do
   @captcha = RestfulCaptcha::Captcha.new(params)
   @captcha.identifier
 end
 
-# Given a captcha identifier, returns the same identifier in the
-# response body with an OK status; or responds with a 'resource not
-# found' error if the captcha cannot be found.
-#
-# This is useful for clients wanting to verify that an identifier is
-# valid.
 get '/captcha/:identifier' do
   @captcha = RestfulCaptcha::Captcha.find(params[:identifier])
   if @captcha.nil?
@@ -33,7 +73,6 @@ get '/captcha/:identifier' do
   @captcha.identifier
 end
 
-# Given a captcha identifier, returns the captcha's image.
 get '/captcha/:identifier/image' do
   @captcha = RestfulCaptcha::Captcha.find(params[:identifier])
   if @captcha.nil?
